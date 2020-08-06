9f10717d3c76221153a3a920feec6e563f0630bc
Stephen Anderson <stephenx11x@icloud.com>
2020-08-06 01:27:09 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -58,8 +58,7 @@ def runner
   welcome
   sum = initial_round
   until sum > 21 do
-    hit?(sum)
-    display_card_total(sum)
+    display_card_total(hit?(sum))
     end
   if sum > 21
     end_game(sum)
